ed072066f68ff421c6d10df9bc5412870bfbb1c4
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2020-11-05 12:02:13 +0800

    v0.1

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+ 
+setup(name='EventLab',
+      version="0.1",
+      description='A SDK for Event Cameras',
+      author='HarmoniaLeo',
+      author_email='1532282692@qq.com',
+      maintainer='HarmoniaLeo',
+      maintainer_email='1532282692@qq.com',
+      url='https://harmonialeo.github.io/EventLab/',
+      packages=['EventLab','EventLab.Objects','EventLab.Algorithms'],
+      license="Public domain",
+      platforms=["any"],
+     )
